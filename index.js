65e55ce79d82901e2f00aea3402cfe7366441eae
Trần Đình Nghĩa <trandinhnghia555@gmail.com>
2023-08-22 21:24:10 +0700

    'update'

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ var timeNow = new Date();
 var countDate = new Date('feb 10, 2024 00:00:00').getTime();
 let music = new Audio;
 
-// let width = window.screen.availWidth;
-// let height = window.screen.width;
-
 music.src = "/public/y2mate.com - Dan Gibson  A Breath of Spring.mp3"
 music.loop = true;
 
+// Hàm tạo sao băng
+function creartMeteor() {
+    let container = document.querySelector('#container'); 
+    let widthContainer = container.offsetWidth;
+    let meteor = document.createElement('span');
+    let randomLeft = Math.floor(Math.random() * widthContainer);
+    meteor.classList.add('meteor');
+    meteor.style.left = randomLeft + 'px';
+    container.appendChild(meteor);
+
+
+    setTimeout(function () { 
+        container.removeChild(meteor)
+    },10000);
+}
+
+
+
 setInterval(countDown , 1000);
-// playAudio();
+
 function playAudio() {
     Swal.fire({
         title: 'Bạn có muốn thưởng thức âm nhạc không!',
@@ -46,30 +61,6 @@ function getDay() {
 
 }
 
-// let chooseDay = document.getElementById('selectDay');
-
-// chooseDay.addEventListener('change', (event) => {
-//         var day = chooseDay.value;
-//         var nameDay = event.target.options[event.target.selectedIndex].text;
-//         checkDay(day,nameDay);
-// })
-
-// function checkDay(day,nameDay) {
-//     let dd = new Date(day);
-//         var a1 = (dd.getMonth() + 1) +'/'+ dd.getDate() + '/' + timeNow.getFullYear();
-        
-//         var a3 = new Date(a1);
-
-//         if(a3 < timeNow) {
-//             a1 = (dd.getMonth() + 1) +'/'+ dd.getDate() + '/' + (timeNow.getFullYear()+1);
-//             var days = new Date(a1).getTime();
-//             notificationDay(days,nameDay);
-            
-//         }else {
-//             var days = new Date(a1).getTime();
-//             notificationDay(days,nameDay);
-//         }
-// }
 
 function countDown() {
     let now = new Date().getTime();
@@ -91,44 +82,16 @@ function countDown() {
     document.getElementById('seconds').innerHTML = s;
 }
 
-// function notificationDay(days,nameDay) {
-//     let now = new Date().getTime();
-//     let gap = days - now;
-
-//     let second = 1000;
-//     let minute = second * 60;
-//     let hour = minute * 60;
-//     let day = hour * 24;
-
-//     let d = Math.floor(gap / (day));
-//     let h = Math.floor(gap % (day) / (hour));
-//     let m = Math.floor(gap % (hour) / (minute));
-//     let s = Math.floor(gap % (minute) / (second));
-
-//     let timerInterval
-//     Swal.fire({
-//     html: "Còn " + d + " ngày nữa đến " + nameDay + ".",
-//     timer: 2000,
-//     timerProgressBar: true,
-//     didOpen: () => {
-//         Swal.showLoading()
-//         const b = Swal.getHtmlContainer().querySelector('b')
-//     },
-//     willClose: () => {
-//         clearInterval(timerInterval)
-//     }
-//     }).then((result) => {
-//     /* Read more about handling dismissals below */
-//     })
-// }
-
 $(document).ready(function () {
     $("#button-play").on('click', function() {
         $("#container").css({
             "top": "0%",
             "opacity": "1"
         });
-        // music.play();
+        music.play();
+        setInterval(function() {
+            creartMeteor()
+        },1000);
     })
 
     $("#button-pause").on('click', function() {
@@ -136,6 +99,9 @@ $(document).ready(function () {
             "top": "100%",
             "opacity": "0"
         });
-        // music.pause();
+        music.pause();
+        clearInterval(function() {
+            creartMeteor()
+        },1000);
     })
 });
